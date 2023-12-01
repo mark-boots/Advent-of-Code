ca123e303e893f49e014e33ecbed70c1b83c5eaf
Mark Boots <info@mark-boots.nl>
2023-12-01 11:54:19 +0100

    Update index.js

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -1,12 +1,11 @@
 import loadLines from '../../LoadLines.js'
 
 const lines = loadLines('input.txt');
-const numbers = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+const nums = ['zero','one','two','three','four','five','six','seven','eight','nine','\\d'];
 
 const result = lines.reduce((sum,line)=>{
-  let matches = Array.from(line.matchAll(`(?=(${numbers.join("|")}|\\d))`)).map(m=>m[1].match(/\d/) ? m[1] : numbers.indexOf(m[1]) || m[1]);
+  const matches = Array.from(line.matchAll(`(?=(${nums.join("|")}))`)).map(m=>m[1].match(/\d/) ? m[1] : nums.indexOf(m[1]) || m[1]);
   return sum += matches.at(0) * 10 + matches.at(-1) * 1;
 }, 0)
 
-console.log(result)
-
+console.log(result);
