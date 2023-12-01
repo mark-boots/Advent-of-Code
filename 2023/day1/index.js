2e4870a380566aa926aa1d28649555623bc0318f
Mark Boots <info@mark-boots.nl>
2023-12-01 11:43:15 +0100

    Update index.js

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -2,13 +2,10 @@ import loadLines from '../../LoadLines.js'
 
 const lines = loadLines('input.txt');
 const numbers = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+
 const result = lines.reduce((sum,line)=>{
-  let matches = Array.from(line.matchAll(`(?=(${numbers.join("|")}|\\d))`)).map(m=>m[1]);
-  let first = matches.at(0);
-  let last = matches.at(-1);
-  first = first.match(/\d/) ? first : numbers.indexOf(first);
-  last = last.match(/\d/) ? last : numbers.indexOf(last);
-  return sum += first * 10 + last * 1;
+  let matches = Array.from(line.matchAll(`(?=(${numbers.join("|")}|\\d))`)).map(m=>m[1].match(/\d/) ? m[1] : numbers.indexOf(m[1]) || m[1]);
+  return sum += matches.at(0) * 10 + matches.at(-1) * 1;
 }, 0)
 
 console.log(result)
