1ee6e2bc41d5ba216adee0ccba40eabd4c465fa0
Mark Boots <info@mark-boots.nl>
2023-12-02 10:18:34 +0100

    Update index.js

--- 2023/day2/index.js
+++ 2023/day2/index.js
@@ -13,14 +13,13 @@ const games = lines.reduce((games, line) => {
     });
     games.push(game);
     return games;
-},[]);
+}, []);
 console.log(games);
 
 //part1
-const part1 = games.filter(game => game.red <= 12 && game.green <=13 && game.blue <= 14)
-.reduce((sum,game)=> sum += game.gameId, 0);
+const part1 = games.reduce((sum, game) => sum += (game.red <= 12 && game.green <= 13 && game.blue <= 14) ? game.gameId : 0, 0);
 console.log(part1);
 
 //part2
-const part2 = games.reduce((sum,game) => sum += game.red * game.green * game.blue, 0);
+const part2 = games.reduce((sum, game) => sum += game.red * game.green * game.blue, 0);
 console.log(part2);
