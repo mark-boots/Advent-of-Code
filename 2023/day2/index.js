e678d48fae80cbc899a47a52670e43c20b770d25
Mark Boots <info@mark-boots.nl>
2023-12-02 09:53:15 +0100

    2023 - day 2

---
+++ 2023/day2/index.js
@@ -0,0 +1,26 @@
+import loadLines from '../../LoadLines.js'
+
+const lines = loadLines('input.txt');
+const games = lines.reduce((games, line) => {
+    const [id, reaches] = line.split(": ");
+    const gameId = Number(id.split(" ")[1]);
+    const game = {gameId, "red": 0, "green": 0, "blue": 0};
+    reaches.split("; ").forEach(reach => {
+        reach.split(", ").forEach(color => {
+            const [amount, name] = color.split(" ");
+            if(game[name] < Number(amount)) game[name] = Number(amount);
+        });
+    });
+    games.push(game);
+    return games;
+},[]);
+console.log(games);
+
+//part1
+const part1 = games.filter(game => game.red <= 12 && game.green <=13 && game.blue <= 14)
+.reduce((sum,game)=> sum += game.gameId, 0);
+console.log(part1);
+
+//part2
+const part2 = games.reduce((sum,game) => sum += game.red * game.green * game.blue, 0);
+console.log(part2);
