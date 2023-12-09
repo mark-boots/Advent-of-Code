9945112f76c45a5633636c49c5b984f4cd1c4c4d
Mark Boots <info@mark-boots.nl>
2023-12-09 10:29:50 +0100

    Update index.js

--- 2023/day9/index.js
+++ 2023/day9/index.js
@@ -1,17 +1,15 @@
 import loadLines from '../../LoadLines.js'
 
-const [part1, part2] = loadLines('input.txt').reduce((result, history) => {
+const [part1, part2] = loadLines('input.txt').reduce((results, history) => {
     let sequence = history.split(" ").map(Number);
     const vals = [sequence.at(-1), sequence[0]];
     let i = 0;
-    while(!sequence.every(v => v == 0)) {
+    while(sequence.some(v => v !== 0)) {
         sequence = sequence.slice(1).map((val, idx) => val - sequence[idx]);
         vals[0] += sequence.at(-1);
         vals[1] += sequence[0] * [1,-1][++i % 2];
     }
-    result[0] += vals[0];
-    result[1] += vals[1];
-    return result;
+    return results.map((result, i) => result + vals[i]);
 }, [0,0])
 
 console.log({ part1, part2 }) // { part1: 1868368343, part2: 1022 }
