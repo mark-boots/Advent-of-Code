f99ecb0666e493153a3b2d59da778bc41238b301
Mark Boots <info@mark-boots.nl>
2023-12-10 17:36:55 +0100

    updated 2023 day 10
    part 1 + 2 together

--- 2023/day10/index.js
+++ 2023/day10/index.js
@@ -12,15 +12,42 @@ const directions = {
 
 let part1 = 0;
 let [pos1, pos2] = getStartPositions();
+let seen = new Set();
+seen.add(posToString(pos1));
+
 while(true){
     pos1 = walk(pos1);
     pos2 = walk(pos2);
+    seen.add(posToString(pos1))
+    seen.add(posToString(pos2))
+
     part1++;
-    if(pos1.y == pos2.y && pos1.x == pos2.x) break;
+    if(pos1.y == pos2.y && pos1.x == pos2.x) {
+        seen.add(posToString(pos1))
+        break;
+    }
 }
-console.log(part1)
+
+const lx = map[0].length / 4
+const ly = map.length / 4
+const part2 = sumArray( 
+    map
+    .slice(ly, -ly)
+    .map((line, y) => line
+      .slice(lx, -lx)
+      .filter((_, x) => !seen.has(posToString({x: x+lx, y: y+ly})))
+      .length
+    )
+)
+
+console.log({
+    part1,
+    part2,
+});
+
 
 // helpers
+
 function getStartPositions(){
     const startIdx = lines.join("").indexOf("S");
     const pos = { y: Math.floor(startIdx / lines[0].length), x: startIdx % lines[0].length };
@@ -31,8 +58,8 @@ function getStartPositions(){
         ['left',map[pos.y][pos.x-1]],
     ].filter((dir,i) => Object.values(directions)[i][dir[1]])
     .map(v=>v[0]);
-    let pos1 = {y: pos.y, x: pos.x, dir: dir1};
-    let pos2 = {y: pos.y, x: pos.x, dir: dir2};
+    let pos1 = {y: pos.y, x: pos.x, dir: dir1, val: map[pos.y][pos.x]};
+    let pos2 = {y: pos.y, x: pos.x, dir: dir2, val: map[pos.y][pos.x]};
     return [pos1, pos2];
 }
 
@@ -43,7 +70,11 @@ function walk({y,x,dir}){
     if(dir == 'down') { newY = y + 1 };
     if(dir == 'left') { newX = x - 1 }
     
-    let newDir = directions[dir][map[newY][newX]]
-    let newPos = {y:newY, x:newX, dir:newDir};
+    let newDir = directions[dir][map[newY][newX]];
+    let newVal = map[newY][newX];
+    let newPos = {y:newY, x:newX, dir:newDir, val: newVal};
     return newPos;
 }
+
+function posToString({y,x}){ return y+","+x }
+function sumArray (arr){ return arr.reduce((a,v)=> a+v) }
