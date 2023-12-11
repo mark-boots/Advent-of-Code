06e9c48e9d14be3981daa07368b1fb7d71d2f452
Mark Boots <info@mark-boots.nl>
2023-12-11 17:31:23 +0100

    Update version2.js

--- 2023/day11/version2.js
+++ 2023/day11/version2.js
@@ -2,25 +2,28 @@ import loadLines from '../../LoadLines.js';
 
 const space = loadLines('input.txt').map(line => line.split(""));
 const galaxies = [];
-const pairs = [];
 const emptyRows = [];
 const emptyCols = [];
+const pairDistances = [];
 
 for(let y = 0, max = Math.max(space.length, space[0].length); y < max; y++){
     let emptyRow = true;
     let emptyCol = true;
     for(let x = 0; x < max; x++){
-        //galaxies, pairs and empty rows;
+        //empty rows, galaxies and pairDistances;
         if (x < space[0].length && space[y][x] == '#'){
             emptyRow = false;
             galaxies.push({y,x});
             if(galaxies.length > 1) {     
                 for(let i = 0; i < galaxies.length -1; i++){
-                    pairs.push([galaxies.at(-1), galaxies[i]]);
+                    const [yMin, yMax] = [galaxies[i].y, y].sort((a,b)=>a-b);
+                    const [xMin, xMax] = [galaxies[i].x, x].sort((a,b)=>a-b);
+                    const dist = (yMax - yMin) + (xMax - xMin);
+                    pairDistances.push({yMin, yMax, xMin, xMax, dist});                    
                 }
             }
         }
-        //empty cols; (swap x y index)
+        //empty cols; (swap x y index);
         if(y < space.length && space[x][y] !=='.') emptyCol = false;
     }
     if(emptyCol) emptyCols.push(y);
@@ -32,15 +35,11 @@ console.log({
     part2: cosmicExpansion(1000000), //717878258016
 })
 
-function cosmicExpansion(expansion){
-    return pairs.reduce((sum, pair) => sum + getDistance(pair, expansion), 0);
+function cosmicExpansion(factor){
+    return pairDistances.reduce((sum, distance) => sum + expandEmptySpace(distance, factor), 0);
 }
-function getDistance([p1,p2], expansion){
-    const [yMin, yMax] = [p1.y, p2.y].sort((a,b) => a - b)
-    const [xMin, xMax] = [p1.x, p2.x].sort((a,b) => a - b)
-    const emptyRowsBetween = emptyRows.filter(v => v > yMin && v < yMax).length;
-    const emptyColsBetween = emptyCols.filter(v => v > xMin && v < xMax).length;
-    const distanceY = yMax - yMin + (emptyRowsBetween * (expansion - 1));
-    const distanceX = xMax - xMin + (emptyColsBetween * (expansion - 1));
-    return distanceY + distanceX;
+function expandEmptySpace({yMin, yMax, xMin, xMax, dist}, factor){
+    const er = emptyRows.filter(v => v > yMin && v < yMax).length * (factor - 1);
+    const ec = emptyCols.filter(v => v > xMin && v < xMax).length * (factor - 1);
+    return dist + er + ec;
 }
