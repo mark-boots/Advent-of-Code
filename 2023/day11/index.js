c873e4dc51ccb849a6bdd64520e59d1b193f6e7f
Mark Boots <info@mark-boots.nl>
2023-12-15 16:14:28 +0100

    update day 11

--- 2023/day11/index.js
+++ 2023/day11/index.js
@@ -1,62 +1,42 @@
 import loadLines from '../../LoadLines.js';
-
 const space = loadLines('input.txt').map(line => line.split(""));
-const galaxies = findPositions(space, "#");
-const galaxyPairs = getPairs(galaxies);
-const emptyRows = getEmptyRows(space);
-const emptyCols = getEmptyCols(space);
+const galaxies = [];
+const emptyRows = [];
+const emptyCols = [];
+const pairDistances = [];
 
-console.log({
-    part1: cosmicExpansion(2),       // 9693756
-    part2: cosmicExpansion(1000000), // 7178258016
-})
-
-function cosmicExpansion(expansion){
-    return galaxyPairs.reduce((sum, pair) => sum + getDistance(pair, expansion), 0);
-}
-function getDistance([p1,p2], expansion){
-    const [yMin, yMax] = [p1.y, p2.y].sort((a,b) => a - b)
-    const [xMin, xMax] = [p1.x, p2.x].sort((a,b) => a - b)
-    const emptyRowsInRange = emptyRows.filter(v => v > yMin && v < yMax).length;
-    const emptyColsInRange = emptyCols.filter(v => v > xMin && v < xMax).length;
-    const distanceY = yMax - yMin - emptyRowsInRange + (emptyRowsInRange * expansion);
-    const distanceX = xMax - xMin - emptyColsInRange + (emptyColsInRange * expansion);
-    return distanceY + distanceX;
-}
-function getEmptyRows(arr){
-    const temp = [];
-    for(let y = 0; y < arr.length; y++){
-        if(arr[y].every(v => v==='.')) temp.push(y);
-    }
-    return temp;
-}
-function getEmptyCols(arr){
-    const temp = [];
-    for(let x = 0; x < arr[0].length; x++){
-        let empty = true;
-        for(let y = 0; y < arr.length; y++){
-            if(arr[y][x] !== '.') {
-                empty = false;
-                break;
+for(let y = 0, max = Math.max(space.length, space[0].length); y < max; y++){
+    let emptyRow = true;
+    let emptyCol = true;
+    for(let x = 0; x < max; x++){
+        //empty rows, galaxies and pairDistances;
+        if (x < space[0].length && space[y][x] == '#'){
+            emptyRow = false;
+            galaxies.push({y,x});
+            if(galaxies.length > 1) {     
+                for(let i = 0; i < galaxies.length -1; i++){
+                    const [yMin, yMax] = [galaxies[i].y, y].sort((a,b)=>a-b);
+                    const [xMin, xMax] = [galaxies[i].x, x].sort((a,b)=>a-b);
+                    const dist = (yMax - yMin) + (xMax - xMin);
+                    pairDistances.push({yMin, yMax, xMin, xMax, dist});                    
+                }
             }
         }
-        if(empty) temp.push(x)
+        //empty cols; (swap x y index);
+        if(y < space.length && space[x][y] !=='.') emptyCol = false;
     }
-    return temp;
+    if(emptyCol) emptyCols.push(y);
+    if(emptyRow) emptyRows.push(y);    
 }
-function getPairs (arr) {
-    return arr.flatMap((v1, i1) => {
-        return arr.flatMap((v2, i2) => { 
-            return (i1 > i2) ? [[v1,v2]] : []
-        })
-    })
-}
-function findPositions(arr, find){
-    const temp = [];
-    for(let y = 0; y < arr.length; y++){
-        for(let x = 0; x < arr[y].length; x++){
-            if (arr[y][x] == find) temp.push({y,x})
-        }
-    }
-    return temp;
+
+console.log({
+    part1: cosmicExpansion(2),       //9693756
+    part2: cosmicExpansion(1000000), //717878258016
+})
+function cosmicExpansion(factor){
+    return pairDistances.reduce((sum, {yMin, yMax, xMin, xMax, dist}) => {
+        const er = emptyRows.filter(v => v > yMin && v < yMax).length * (factor - 1);
+        const ec = emptyCols.filter(v => v > xMin && v < xMax).length * (factor - 1);
+        return sum + dist + er + ec;
+    }, 0)
 }
