59b14d964fa4f7189f53f46f3f33c34a1cbe156f
Mark Boots <info@mark-boots.nl>
2023-12-16 13:39:50 +0100

    Update index.js

--- 2023/day16/index.js
+++ 2023/day16/index.js
@@ -1,19 +1,12 @@
 import loadLines from '../../LoadLines.js'
 
 const GRID = loadLines('input.txt').map(v=>v.split(""));
-const GRID_H = GRID.length;
-const GRID_W = GRID[0].length;
+const [ GRID_H, GRID_W ] = [ GRID.length, GRID[0].length ];
 const withinGrid = (y, x) => y >= 0 && y < GRID_H && x >= 0 && x < GRID_W;
 
 const beams = [];
-for(let y = 0; y < GRID_H; y++){
-    beams.push([ [y, -1, 1] ]);
-    beams.push([ [y, GRID_W, 3] ]);
-}
-for(let x = 0; x < GRID_W; x++){
-    beams.push([ [-1, x, 2] ]);
-    beams.push([ [GRID_H, x, 0] ]);
-}
+for(let y = 0; y < GRID_H; y++) beams.push( [y, -1, 1], [y, GRID_W, 3] );
+for(let x = 0; x < GRID_W; x++) beams.push( [-1, x, 2], [GRID_H, x, 0] );
 const beamResults = beams.map(getEnergizedTiles);
 
 console.log({ 
@@ -21,7 +14,8 @@ console.log({
     part2: Math.max(...beamResults) // 7594
 });
 
-function getEnergizedTiles(beam) {
+function getEnergizedTiles(beamStart) {
+    const beam = [ beamStart ];
     const seen = new Set();
     const energized = new Set();
     while(beam.length > 0){
@@ -33,11 +27,11 @@ function getEnergizedTiles(beam) {
         seen.add(key);
         energized.add(`${yy},${xx}`);
         let pos = GRID[yy][xx];
-        if(pos == '/') beam.push([yy, xx, (5-d)%4]);
-        else if(pos == '\\') beam.push([yy, xx, 3-d]);
-        else if(pos == '|' && [1, 3].includes(d)) beam.push([yy, xx, 0], [yy, xx, 2]);
-        else if(pos == '-' && [0, 2].includes(d)) beam.push([yy, xx, 1], [yy, xx, 3]);
-        else beam.push([yy, xx, d]);
+        if(pos == '/') beam.push( [yy, xx, (5-d)%4] );
+        else if(pos == '\\') beam.push( [yy, xx, 3-d] );
+        else if(pos == '|' && [1, 3].includes(d)) beam.push( [yy, xx, 0], [yy, xx, 2] );
+        else if(pos == '-' && [0, 2].includes(d)) beam.push( [yy, xx, 1], [yy, xx, 3] );
+        else beam.push( [yy, xx, d] );
     }
     return energized.size;
 }
