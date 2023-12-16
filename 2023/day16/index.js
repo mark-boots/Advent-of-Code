9db81e91f0a949c2107c8a73595eec172b93d4ee
Mark Boots <info@mark-boots.nl>
2023-12-16 10:34:26 +0100

    Update index.js

--- 2023/day16/index.js
+++ 2023/day16/index.js
@@ -19,28 +19,27 @@ console.log({
     part1: beamResults[0],          // 6921
     part2: Math.max(...beamResults) // 7594
 });
-
 function getEnergizedTiles(beam) {
     const seen = new Set();
     const energized = new Set();
     while(beam.length > 0){
         let [y, x, d] = beam.shift();
         const key = [y,x,d].join(",");
-        if(seen.has(key)) continue;
-        seen.add(key);
         const [yy, xx] = [[y-1, x], [y, x+1], [y+1, x], [y, x-1]][d];
-    
-        if (yy < 0 || yy >= GRID_H || xx < 0 || xx >= GRID_W ) continue;
-        let pos = GRID[yy][xx];
+        
+        if (seen.has(key) || yy < 0 || yy >= GRID_H || xx < 0 || xx >= GRID_W ) continue;
+        seen.add(key);
         energized.add(`${yy},${xx}`);
+
+        let pos = GRID[yy][xx];
         if(pos == '/') {
-            beam.push([yy, xx, [1,0,3,2][d]]);
+            beam.push([yy, xx, [1, 0, 3, 2][d]]);
         } else if(pos == '\\') {
-            beam.push([yy, xx, [3,2,1,0][d]]);
-        } else if(pos == '|' && (d == 1 || d == 3)){
+            beam.push([yy, xx, 3-d]);
+        } else if(pos == '|' && [1, 3].includes(d)){
             beam.push([yy, xx, 0]);
             beam.push([yy, xx, 2]);
-        } else if(pos == '-' && (d == 0 || d == 2)){
+        } else if(pos == '-' && [0, 2].includes(d)){
             beam.push([yy, xx, 1]);
             beam.push([yy, xx, 3]);
         } else {
