4df64373820447ce82d4c9d039776b921d95a172
Mark Boots <info@mark-boots.nl>
2024-12-01 11:31:41 +0100

    Update LoadLines.js

--- LoadLines.js
+++ LoadLines.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 
-export default function loadLines(fileName){
-    const lines = fs.readFileSync(fileName, 'utf8').split('\r\n');
-    return lines;  
+export default function loadLines(fileName, split = true){
+    const lines = fs.readFileSync(fileName, 'utf8');
+    if(!split) return lines;  
+    return lines.split('\r\n')
 }
