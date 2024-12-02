8f11bc53bb93cb28116efd8af9e480bfaffd26ef
Mark Boots <info@mark-boots.nl>
2024-12-02 07:39:04 +0100

    Update index.js

--- 2024/day1/index.js
+++ 2024/day1/index.js
@@ -1,24 +1,18 @@
 import loadLines from '../../LoadLines.js';
 const lines = loadLines("input.txt")
 
-// prepare
-const [left, right] = rows2cols(lines.map(l=>l.split(/\s+/g)))
+// load + prepare
+const [left, right] = rows2cols(lines.map(l=>l.split(/\s+/g))).map(l => l.sort())
 
 // part 1 + 2
 const solution = { part1: 0, part2: 0 };
 for(let i = 0; i < left.length; i++){
-  solution.part1 += Math.abs(smallest(left, i) - smallest(right, i));
-  solution.part2 += left[i] * count(right, left[i]);
+  solution.part1 += Math.abs(left[i] - right[i]);
+  solution.part2 += left[i] * right.filter(v => v == left[i]).length;
 }
 console.log(solution) // { part1: 2430334, part2: 28786472 }
 
 // helper functions
 function rows2cols(arr){
   return Array.from({ length: arr[0].length }, (_, col) => arr.map(row => row[col]));
-}
-function smallest(arr, nth = 0){
-  return arr.sort((a, b) => a - b)[nth];
-}
-function count(arr, val){
-  return arr.filter(v => v==val).length;
 }
