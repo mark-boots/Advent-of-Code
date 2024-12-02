e2271e12da7c79a547b23b0eaa2f71b7986185d0
Mark Boots <info@mark-boots.nl>
2024-12-02 18:06:15 +0100

    Update index.js

--- 2024/day2/index.js
+++ 2024/day2/index.js
@@ -1,5 +1,3 @@
-// https://adventofcode.com/2024/day/2
-
 // load + prepare
 import loadLines from '../../LoadLines.js';
 const reports = loadLines("input.txt").map(line => line.split(" ").map(Number));
@@ -16,12 +14,6 @@ solution.part2 = reports.filter(report => {
 }).length
 
 console.log(solution) //{ part1: 257, part2: 328 }
-
-// function isSafeReport(report){
-//   if(report.slice(1).every((level, idx) => level - report[idx] <= 3 && level - report[idx] >= 1 )) return true
-//   if(report.slice(1).every((level, idx) => report[idx] - level <= 3 && report[idx] - level >= 1 )) return true
-//   return false
-// }
 
 function isSafeReport(report) {
   const diff = report.slice(1).map((level, idx) => level - report[idx]);
