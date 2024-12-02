d9f8102f0d39e0ad2d226781bf7c4cf79094bb28
Mark Boots <info@mark-boots.nl>
2024-12-02 18:05:03 +0100

    Update index.js

--- 2024/day2/index.js
+++ 2024/day2/index.js
@@ -1,9 +1,10 @@
 // https://adventofcode.com/2024/day/2
-import loadLines from '../../LoadLines.js';
 
 // load + prepare
+import loadLines from '../../LoadLines.js';
 const reports = loadLines("input.txt").map(line => line.split(" ").map(Number));
 
+// part 1 + 2
 const solution = { part1: 0, part2: 0 };
 
 solution.part1 = reports.filter(isSafeReport).length
@@ -16,8 +17,13 @@ solution.part2 = reports.filter(report => {
 
 console.log(solution) //{ part1: 257, part2: 328 }
 
-function isSafeReport(report){
-  if(report.slice(1).every((level, idx) => level - report[idx] <= 3 && level - report[idx] >= 1 )) return true
-  if(report.slice(1).every((level, idx) => report[idx] - level <= 3 && report[idx] - level >= 1 )) return true
-  return false
+// function isSafeReport(report){
+//   if(report.slice(1).every((level, idx) => level - report[idx] <= 3 && level - report[idx] >= 1 )) return true
+//   if(report.slice(1).every((level, idx) => report[idx] - level <= 3 && report[idx] - level >= 1 )) return true
+//   return false
+// }
+
+function isSafeReport(report) {
+  const diff = report.slice(1).map((level, idx) => level - report[idx]);
+  return diff.every(d => d >= 1 && d <= 3) || diff.every(d => d <= -1 && d >= -3);
 }
