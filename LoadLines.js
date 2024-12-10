081e21e125b050e3981e7a3b06d3d7fc0e5ac4cb
Mark Boots <info@mark-boots.nl>
2024-12-10 07:31:06 +0100

    Remove ignored files

--- LoadLines.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-export default function loadLines(fileName, split = true){
-    const lines = fs.readFileSync(fileName, 'utf8');
-    if(!split) return lines;  
-    return lines.split('\r\n')
-}
