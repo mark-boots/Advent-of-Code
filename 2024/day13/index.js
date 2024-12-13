546d07d42d375e9fcb5e7c948cb27f87015d19fd
Mark Boots <info@mark-boots.nl>
2024-12-13 07:36:17 +0100

    Update index.js

--- 2024/day13/index.js
+++ 2024/day13/index.js
@@ -2,10 +2,7 @@
 import loadLines from '../../LoadLines.js';
 const machines = loadLines("input.txt", false).split("\r\n\r\n")
 
-console.log({
-  part1: solve(machines),
-  part2: solve(machines, 1e13)
-})
+console.log({ part1: solve(machines), part2: solve(machines, 1e13) })
 
 function solve(machines, adjust = 0) {
   return machines.reduce((tokens, machine) => {
@@ -16,14 +13,9 @@ function solve(machines, adjust = 0) {
 
 // linear Diophantine equations using Cramer's rule to solve
 function minimumTokens(ax, ay, bx, by, px, py) { 
-
   const det = ax * by - ay * bx;
   if (det === 0) return 0;
-
   const nA = (by * px - bx * py) / det;
   const nB = (ax * py - ay * px) / det;
-
-  return (Number.isInteger(nA) && Number.isInteger(nB))
-    ? nA * 3 + nB * 1
-    : 0;
+  return (Number.isInteger(nA) && Number.isInteger(nB)) ? nA * 3 + nB * 1 : 0;
 }
