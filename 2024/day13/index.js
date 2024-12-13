9e06c441cdc7920339eeeacfb542d35d4da030bf
Mark Boots <info@mark-boots.nl>
2024-12-13 11:33:44 +0100

    update day 13

--- 2024/day13/index.js
+++ 2024/day13/index.js
@@ -1,12 +1,10 @@
-// load + prepare
 import loadLines from '../../LoadLines.js';
 const machines = loadLines("input.txt", false).split("\r\n\r\n").map(m => m.match(/\d+/g))
 const solve = (adjust = 0) => 
   machines.reduce((tokens, [ax, ay, bx, by, tx, ty]) => {
     [tx, ty] = [tx, ty].map(v => +v + adjust);
-    const d = ax * by - ay * bx;
-    const a = (by * tx - bx * ty) / d;
-    const b = (ax * ty - ay * tx) / d;
-    return tokens + ((a%1 == 0 && b%1 == 0) ? a*3 + b : 0);
+    const a = (by * tx - bx * ty) / (ax * by - ay * bx);
+    const b = (ax * ty - ay * tx) / (ax * by - ay * bx);
+    return tokens + ( !(a%1||b%1) ? a*3 + b : 0 );
   }, 0)
 console.log({ part1: solve(), part2: solve(1e13) })
