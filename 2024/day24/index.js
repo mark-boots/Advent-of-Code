8e082ac0b9ba5e9ab57c1f11374b873894457f16
Mark Boots <info@mark-boots.nl>
2024-12-24 13:19:50 +0100

    Update index.js

--- 2024/day24/index.js
+++ 2024/day24/index.js
@@ -1,19 +1,8 @@
 import loadLines from '../../LoadLines.js';
-const input = loadLines('input.txt', false).trim().split('\r\n\r\n').map(lines => lines.split('\r\n'));
-const wires = input[0].reduce((obj, wire) =>{
-    const [key, val] = wire.split(': ');
-    obj[key] = parseInt(val);
-    return obj
-},{})
-const instructions = input[1].map(instruction => {
-    const [in1, op, in2, _, out] = instruction.split(' ');
-    return { in1, op, in2, out, executed: false }
-})
-const operation = {
-    'AND': (a, b) => a & b,
-    'OR': (a, b) => a | b,
-    'XOR': (a, b) => a ^ b
-}
+const input = loadLines('input.txt', false).trim().split('\r\n\r\n').map(lines => lines.split('\r\n').map(l => l.split(/:?\s/g)));
+const wires = input[0].reduce((obj, [key, val]) => (obj[key] = parseInt(val), obj), {})
+const instructions = input[1].map(([in1, op, in2, _, out]) => ({ in1, op, in2, out, executed: false }));
+const operation = { 'AND': (a, b) => a & b, 'OR': (a, b) => a | b, 'XOR': (a, b) => a ^ b }
 
 console.log({
     part1: part1(wires, instructions),
@@ -21,58 +10,32 @@ console.log({
 })
 
 function part1(wires, instructions){
-    while (instructions.some(instruction => !instruction.executed)) {
-        for (let i = 0; i < instructions.length; i++) {
-            const {in1, op, in2, out, executed} = instructions[i];
-            if (executed) continue;
-
-            if (wires[in1] !== undefined && wires[in2] !== undefined) {
+    while (instructions.some(({ executed }) => !executed)) {
+        instructions.forEach(({in1, op, in2, out, executed}, i) => {
+            if (!executed && wires[in1] !== undefined && wires[in2] !== undefined) {
                 wires[out] = operation[op](wires[in1], wires[in2]);
                 instructions[i].executed = true;
             }
-        }
+        });
     }
-    const zWires = Object.keys(wires).filter(w => w[0] === 'z').sort().reverse().map(w => wires[w]).join('');
-    return parseInt(zWires, 2);
+    return parseInt(Object.keys(wires).filter(w => w[0] === 'z').sort().reverse().map(w => wires[w]).join(''), 2);
 };
 
-/* needed help for this, credits to CodingAP (Github) */ 
-function part2(instructions){
-
-    const XA = (x, id) => instructions.find(({in1, op, in2}) => ((in1 === `x${id}` && in2 === `y${id}`) || (in1 === `y${id}` && in2 === `x${id}`)) && op === x);
-    const Z = (id) => instructions.find(({out}) => out === `z${id}`);
-    const OA = (x) => instructions.find(({in1, in2}) => [in1, in2].includes(x.out));
-
+function part2(instructions) {
     const incorrect = [];
+    const instr = (type, id) => {
+        const m = instructions.find(({ in1, op, in2 }) => ((in1 === `x${id}` && in2 === `y${id}`) || (in1 === `y${id}` && in2 === `x${id}`)) && op === type);
+        return m ? instructions.find(({ in1, in2 }) => [in1, in2].includes(m.out)) : null;
+    };
+    const instrZ = id => instructions.find(({ out }) => out === `z${id}`);
+    const checkConn = (x, ops, first = false) => x && !ops.includes(x.op) && !first ? incorrect.push(x.out) : null;
+    
     for (let i = 0; i < 45; i++) {
         const id = i.toString().padStart(2, '0');
-        const xor = XA("XOR", id);
-        const and = XA("AND", id);
-        const z = Z(id);
-        
-        if([xor, and, z].includes(undefined)) continue
-
-        // each z must be connected to an XOR
-        if (z.op !== 'XOR') incorrect.push(z.out);
-        
-        // each AND must go to an OR (besides the first case as it starts the carry flag)
-        const or = OA(and);
-        if (or && or.op !== 'OR' && i > 0) incorrect.push(and.out);
-
-        // the first XOR must to go to XOR or AND
-        const after = OA(xor);
-        if (after && after.op === 'OR') incorrect.push(xor.out);
+        checkConn(instrZ(id), ['XOR']);
+        checkConn(instr("AND", id), ['OR'], i > 0);
+        checkConn(instr("XOR", id), ['XOR', 'AND']);
     }
-
-    // each XOR must be connected to an x, y, or z
-    incorrect.push(...instructions
-        .filter(({in1, op, in2, out}) => {
-            return  !/[xy]/.test(in1[0]) && 
-                    !/[xy]/.test(in2[0]) && 
-                    !/[z]/.test(out[0]) && 
-                    op === 'XOR'
-        })
-        .map(instruction => instruction.out))
-
+    incorrect.push(...instructions.filter(({ in1, in2, out, op }) => !/[xy]/.test(in1[0]) && !/[xy]/.test(in2[0]) && !/[z]/.test(out[0]) && op === 'XOR').map(i => i.out));
     return incorrect.sort().join(',');
-};
+}
