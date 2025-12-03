4d1437bc37bdec58c522fa936757a96810295531
Mark Boots <markboots@outlook.com>
2025-12-03 10:07:16 +0100

    AoC 2025 - Day 3 - clean up

--- 2025/day3/index.js
+++ 2025/day3/index.js
@@ -1,24 +1,18 @@
 import loadLines from '../../LoadLines.js';
 const input = loadLines("input.txt");
 
-    let part1 = 0, part2 = 0;
-    for(const bank of input){
-        part1 += maxJoltageFromBank(bank, 2);
-        part2 += maxJoltageFromBank(bank, 12);
-    }
-    console.log({ part1, part2 });
+let part1 = 0, part2 = 0;
+for(const bank of input){
+    part1 += maxJoltage(bank, 2);
+    part2 += maxJoltage(bank, 12);
+}
+console.log({ part1, part2 });
 
-    function maxJoltageFromBank(bankDigits, digitsToKeep) {
-    let removalsLeft = bankDigits.length - digitsToKeep;
-    const stack = [];
-
-    for (const digit of bankDigits) {
-        while (removalsLeft > 0 && stack.at(-1) < digit) {
-            stack.pop();
-            removalsLeft--;
-        }
-        stack.push(digit);
+function maxJoltage(bank, count) {
+    let stack = [], remove = bank.length - count;
+    for (const d of bank) {
+        while (remove && stack.at(-1) < d) { stack.pop(); remove--; }
+        stack.push(d);
     }
-
-    return Number(stack.slice(0, digitsToKeep).join(""));
-    }
+    return +stack.slice(0, count).join('');
+}
