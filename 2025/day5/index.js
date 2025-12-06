bc39c41259ff21c34b3a0508ef81785213162717
Mark Boots <markboots@outlook.com>
2025-12-06 10:35:31 +0100

    updated

--- 2025/day5/index.js
+++ 2025/day5/index.js
@@ -1,31 +1,39 @@
 import loadLines from '../../LoadLines.js';
 
+console.time("parse");
 const lines = loadLines("input.txt");
-const ranges = lines.slice(0, lines.indexOf("")).map(l => l.split("-").map(Number))
+const ranges = lines.slice(0, lines.indexOf("")).map(l => l.split("-").map(Number));
 const ids = lines.slice(lines.indexOf("") + 1).map(Number);
+console.timeEnd("parse");
 
-console.log({
-  part1: part1(),
-  part2: part2(),
-})
+console.time("part1");
+console.log({part1: part1()});
+console.timeEnd("part1");
+
+console.time("part2");
+console.log({part2: part2()});
+console.timeEnd("part2");
 
 function part1() {
-  return ids.reduce((total, id) => total + (ranges.find(([start, end]) => id >= start && id <= end)? 1 : 0), 0)
+  return ids.reduce(
+    (total, id) => total + (ranges.find(([start, end]) => id >= start && id <= end) ? 1 : 0),
+    0
+  );
 }
+
 function part2() {
-  let merged = [];
+  const merged = [];
   for (let [start, end] of ranges) {
-    let i = merged.findIndex(([_,mEnd]) => mEnd >= start - 1); // find first index where merged end > range start
-    if (i === -1) merged.push([start, end])  // no inbetween entries. insert end of array
+    let i = merged.findIndex(([_, mEnd]) => mEnd >= start - 1);
+    if (i === -1) merged.push([start, end]);
     else {
-      while (i < merged.length && merged[i][0] <= end + 1) { // if merged start < range end there is overlap
+      while (i < merged.length && merged[i][0] <= end + 1) {
         const [mStart, mEnd] = merged[i];
-        if(mStart < start) start = mStart; //extend left
-        if(mEnd > end) end = mEnd //extend right
-        merged.splice(i, 1); //remove old merged row, next row will get same index for while loop
+        if (mStart < start) start = mStart;
+        if (mEnd > end) end = mEnd;
+        merged.splice(i, 1);
       }
-      //insert new merged start/end at that index.
-      merged.splice(i, 0, [start, end]);  
+      merged.splice(i, 0, [start, end]);
     }
   }
   return merged.reduce((total, [start, end]) => total + end - start + 1, 0);
