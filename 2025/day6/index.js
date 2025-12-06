a6525a3d1c2ce42aff9708093bc8a0eba3c7b2df
Mark Boots <markboots@outlook.com>
2025-12-06 10:35:42 +0100

    AoC 2025 Day 6

---
+++ 2025/day6/index.js
@@ -0,0 +1,20 @@
+import loadLines from '../../LoadLines.js';
+    const g = loadLines("input.txt").map(l => [...l]);
+    const H = g.length, W = g[0].length;
+    const calc = (arr, op) => arr.reduce((a,b)=> op === '+' ? a+ +b : a* +b, op == "+" ? 0 : 1);
+    let part1 = 0, part2 = 0;
+
+    for (let x = 0; x < W;) {
+      if (g.every(r => r[x] === ' ')) { x++; continue; }
+      let rows = Array(H-1).fill(""), cols = [];
+      while (x < W && !g.every(r => r[x] === ' ')) {  
+        for (let y = 0; y < H-1; y++) rows[y] += g[y][x];
+        cols.unshift(g.slice(0,H-1).map(r=>r[x]).join(""));
+        x++;
+      }
+      const op = g[H-1].slice(x-cols.length,x).find(c => "+*".includes(c));
+      part1 += calc(rows, op);
+      part2 += calc(cols, op);
+    }
+
+    console.log({ part1, part2 });
