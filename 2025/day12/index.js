ae04d3f1201ef3e60c147d0c96a5fdc48df260ed
Mark Boots <markboots@outlook.com>
2025-12-12 07:09:03 +0100

    Update index.js

--- 2025/day12/index.js
+++ 2025/day12/index.js
@@ -1,5 +1,5 @@
 import loadLines from '../../LoadLines.js';
-let bs=loadLines('input.txt', false).split('\r\n\r\n');
-let rs=bs.pop().split('\r\n').map(l=>l.split(/\D+/));
-let ss=bs.map(b=>b.match(/#/g).length);
+const bs = loadLines('input.txt', false).split('\r\n\r\n');
+const rs = bs.pop().split('\r\n').map(l=>l.split(/\D+/));
+const ss = bs.map(b=>b.match(/#/g).length);
 console.log(rs.filter(([w,h,...q])=>w*h>=q.reduce((s,c,i)=>s+c*ss[i],0)).length);
